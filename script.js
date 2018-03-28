a1fe11f2f51f89ce677a7189d655a0d94cdc3a09
Kacper Stroisz <kacperstroisz@wp.pl>
2018-03-28 17:37:03 +0200

    Change script.js

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
 var Counter = React.createClass({
-            getInitialState: function() {
-                return {
-                    counter: 0
-                };
-            },
+    getInitialState: function() {
+        return {
+            counter: 0
+        };
+    },
 
-            increment: function() {
-                this.setState({
-                    counter: this.state.counter + 1
-                });
-            },
+    increment: function() {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    },
 
-            decrement: function() {
-                this.setState({
-                    counter: this.state.counter - 1
-                })
-            },
+    decrement: function() {
+        this.setState({
+            counter: this.state.counter - 1
+        })
+    },
 
-            render: function() {
-                return React.createElement('div', { onClick: this.increment },
-                    React.createElement('span', {}, 'Licznik ' + this.state.counter),
-                }
-            });
+    render: function() {
+        return React.createElement('div', {},
+            React.createElement('span', {}, 'Licznik ' + this.state.counter),
+            React.creaetElement('button', { className: 'btn btn-info', onClick: this.increment }),
+            React.creaetElement('button', { className: 'btn btn-info', onClick: this.decrement }),
+        );
+    }
+});
 
-        var element = React.createElement(CountersList); ReactDOM.render(element, document.getElementById('app'));
+var CountersList = React.createClass({
+    render: function() {
+        return
+        React.createElement('div', {},
+            React.creaetElement(Counter, {}),
+            React.creaetElement(Counter, {}),
+            React.creaetElement(Counter, {})
+
+        )
+    }
+
+});
+
+var element = React.createElement(CountersList);
+ReactDOM.render(element, document.getElementById('app'));
